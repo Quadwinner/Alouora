2711e1bf24c19d9f082b26c11a224f081c4b3c13
Quadwinner <shubhamkush012@gmail.com>
2026-01-09 19:11:56 +0530

    feat: Add API utilities index for easier imports

---
+++ src/lib/api/index.ts
@@ -0,0 +1,19 @@
+/**
+ * API Utilities Index
+ *
+ * Centralized exports for all API utilities
+ */
+
+// Response utilities
+export * from './response'
+
+// Error handling
+export * from './errors'
+
+// Validation schemas
+export * from './schemas/common'
+export * from './schemas/user'
+export * from './schemas/product'
+export * from './schemas/cart'
+export * from './schemas/order'
+export * from './schemas/review'
